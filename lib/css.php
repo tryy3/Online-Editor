4d205756363214aff5d043e51fddb0f9c9a22a7f
Tryy3 <Dennistryy3@gmail.com>
2014-04-10 18:28:04 +0200

    extra files!
    lib files and image files!

---
+++ lib/css.php
@@ -0,0 +1,5 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.css">
---
+++ lib/js.php
@@ -0,0 +1,5 @@
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.js"></script>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
